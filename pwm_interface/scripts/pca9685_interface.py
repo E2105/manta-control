90b4cdf720a5bf8f34779d03df03a6bc736eddf8
napahlm <borgepahlm@gmail.com>
2021-03-02 13:29:04 +0100

    Added camera tilt function on joystick

--- pwm_interface/scripts/pca9685_interface.py
+++ pwm_interface/scripts/pca9685_interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from vortex_msgs.msg import Pwm
+from vortex_msgs.msg import Pwm, Manipulator
 import Adafruit_PCA9685
 
 # Constants
@@ -16,6 +16,7 @@ class Pca9685InterfaceNode(object):
     def __init__(self):
         rospy.init_node('pwm_node')
         self.sub = rospy.Subscriber('pwm', Pwm, self.callback, queue_size=1)
+        self.sub_tilt = rospy.Subscriber('manipulator', Manipulator, self.callback, queue_size=1) # servo
 
         self.pca9685 = Adafruit_PCA9685.PCA9685()
         self.pca9685.set_pwm_freq(FREQUENCY)
@@ -27,13 +28,28 @@ class Pca9685InterfaceNode(object):
         rospy.loginfo('Initialized for {0} Hz.'.format(FREQUENCY))
 
     def callback(self, msg):
+        # servo value
+        camera_pwm = 1500
+
         if len(msg.pins) == len(msg.positive_width_us):
             for i in range(len(msg.pins)):
                 if msg.positive_width_us[i] != self.current_pwm[msg.pins[i]]:
                     self.pca9685.set_pwm(msg.pins[i], PWM_ON, self.microsecs_to_bits(msg.positive_width_us[i]))
                     self.current_pwm[msg.pins[i]] = msg.positive_width_us[i]
+
         print(msg.positive_width_us) #troubleshooting
         print(msg.pins)              #troubleshooting
+
+        # Trying out the tilt control
+        servo_tilt = msg.vertical_stepper_direction
+        if servo_tilt == 1:
+            camera_pwm += 10
+        elif servo_tilt == -1:
+            camera_pwm -= 10
+
+        self.current_pwm[msg.pins[8]] = camera_pwm
+            
+
 
     def microsecs_to_bits(self, microsecs):
         duty_cycle_normalized = microsecs / PERIOD_LENGTH_IN_MICROSECONDS
