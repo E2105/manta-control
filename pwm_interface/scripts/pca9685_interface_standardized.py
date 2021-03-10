86ab5ac8fe0d9ff8f4bd8e1a8bc189bdb916a345
napahlm <borgepahlm@gmail.com>
2021-03-10 21:08:05 +0100

    Standardized messaging

--- pwm_interface/scripts/pca9685_interface_standardized.py
+++ pwm_interface/scripts/pca9685_interface_standardized.py
@@ -16,7 +16,16 @@ Pin 9
     Controls the brightness of the lights.
 """
 
-# Constants
+# ROV Constants
+THRUST_RANGE_LIMIT = 100
+NUM_THRUSTERS = rospy.get_param('/propulsion/thrusters/num')
+THRUST_OFFSET = rospy.get_param('/thrusters/offset')
+LOOKUP_THRUST = rospy.get_param('/thrusters/characteristics/thrust')
+LOOKUP_PULSE_WIDTH = rospy.get_param('/thrusters/characteristics/pulse_width')
+THRUSTER_MAPPING = rospy.get_param('/propulsion/thrusters/map')
+THRUSTER_DIRECTION = rospy.get_param('/propulsion/thrusters/direction')
+
+# PWM Board Constants
 PWM_BITS_PER_PERIOD = 4095 #rospy.get_param('/pwm/counter/max')
 FREQUENCY = 50.0 #rospy.get_param('/pwm/frequency/set')
 FREQUENCY_MEASURED = 51.6 #rospy.get_param('/pwm/frequency/measured')
@@ -34,14 +43,16 @@ class Pca9685InterfaceNode(object):
         """
 
         rospy.init_node('pwm_node')
-        self.pwm_sub = rospy.Subscriber('pwm', Float64MultiArray, self.thrusterCallback, queue_size=1)
-        self.dpad_sub = rospy.Subcriber('dpad', Int8MultiArray, self.servoCallback, queue_size=1)
+        self.pwm_sub = rospy.Subscriber('pwm_values', Float64MultiArray, self.thrusterCallback, queue_size=1)
+        self.dpad_sub = rospy.Subscriber('joy/dpad', Int8MultiArray, self.servoCallback, queue_size=1)
 
         self.pca9685 = Adafruit_PCA9685.PCA9685()
         self.pca9685.set_pwm_freq(FREQUENCY)
         self.pca9685.set_all_pwm(0, 0)              # No output on initialization
         self.current_pwm = [0]*16                   # 16 PWM pins
         self.camera_pwm = 1500                      # Servo set to middle position (Looking straight forward)
+        self.cam_pin = 8
+        self.led_pin = 9
 
         rospy.on_shutdown(self.shutdown)
 
@@ -49,27 +60,43 @@ class Pca9685InterfaceNode(object):
 
 
     def thrusterCallback(self, msg):
+        # Writes the PWM signal to the GPIO pin
+        # msg: the array of PWM signals
 
-        if len(msg.pins) == len(msg.positive_width_us):
-            for i in range(len(msg.pins)):
-                if msg.positive_width_us[i] != self.current_pwm[msg.pins[i]]:
-                    self.pca9685.set_pwm(msg.pins[i], PWM_ON, self.microsecs_to_bits(msg.positive_width_us[i]))
-                    self.current_pwm[msg.pins[i]] = msg.positive_width_us[i]
+        if len(NUM_THRUSTERS) == len(msg.data):
+            for pin in range(NUM_THRUSTERS):
+                if msg.data[pin] != self.current_pwm[pin]:
+                    self.pca9685.set_pwm(pin, PWM_ON, self.microsecs_to_bits(msg.data[pin]))
+                    self.current_pwm[msg.data[pin]] = msg.data[pin]
 
-        print(msg.positive_width_us) #troubleshooting
-        print(msg.pins)              #troubleshooting
+        print(msg.data) #troubleshooting
 
 
     def servoCallback(self, msg):
+        # dpad.data[0] --> horizontal dpad
+        # dpad.data[1] --> vertical dpad
+        dpad = msg.data
 
-        servo_tilt = msg.vertical_stepper_direction
-        if servo_tilt == 1:
+        while dpad.data[1] == 1 and dpad.data[0] == 0 and camera_pwm < 1750:
             self.camera_pwm += 5
-        elif servo_tilt == -1:
+        while dpad.data[1] == -1 and dpad.data[1] == 0 and camera_pwm > 1250:
             self.camera_pwm -= 5
 
-        self.pca9685.set_pwm(8, PWM_ON, self.microsecs_to_bits(self.camera_pwm))
-            
+        self.pca9685.set_pwm(cam_pin, PWM_ON, self.microsecs_to_bits(self.camera_pwm))
+
+
+    def ledCallback(self, msg):
+        # dpad.data[0] --> horizontal dpad
+        # dpad.data[1] --> vertical dpad
+        dpad = msg.data
+
+        while dpad.data[0] == 1 and dpad.data[1] == 0 and brightness < 1900:
+            self.birghtness += 10
+        while dpad.data[0] == -1 and dpad.data[1] == 0 and brightness > 1100:
+            self.brightness -= 10
+
+        self.pca9685.set_pwm(led_pin, PWM_ON, self.microsecs_to_bits(self.brightness))
+
 
     def microsecs_to_bits(self, microsecs):
         duty_cycle_normalized = microsecs / PERIOD_LENGTH_IN_MICROSECONDS
