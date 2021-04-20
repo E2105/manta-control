f30447c0842ce056293be6382a45cb64593d55a1
napahlm <borgepahlm@gmail.com>
2021-04-20 12:07:52 +0200

    Fixed publishing ROV location in obs_interface and added launch file

--- vortex_estimator/include/vortex_estimator/simple_estimator.h
+++ vortex_estimator/include/vortex_estimator/simple_estimator.h
@@ -8,7 +8,7 @@
 
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
-#include "sensor_msgs/FluidPressure.h"
+#include "std_msgs/Float64.h"
 #include "nav_msgs/Odometry.h"
 
 class SimpleEstimator
@@ -16,17 +16,13 @@ class SimpleEstimator
   public:
     SimpleEstimator();
     void imuCallback(const sensor_msgs::Imu &msg);
-    void pressureCallback(const sensor_msgs::FluidPressure &msg);
+    void pressureCallback(const std_msgs::Float64 &msg);
     void publish();
   private:
     ros::NodeHandle m_nh;
     ros::Subscriber m_imu_sub;
-    ros::Subscriber m_pressure_sub;
+    ros::Subscriber m_depth_sub;
     ros::Publisher  m_state_pub;
-
-    double m_atmospheric_pressure;
-    double m_water_density;
-    double m_gravitational_acceleration;
 
     const double c_pi = 3.141592653589793;
 
--- vortex_estimator/src/estimator.cpp
+++ vortex_estimator/src/estimator.cpp
@@ -7,20 +7,11 @@ SimpleEstimator::SimpleEstimator()
 {
   // Initiating topics
   m_imu_sub      = m_nh.subscribe("/sensors/imu/data", 1, &SimpleEstimator::imuCallback, this);
-  m_pressure_sub = m_nh.subscribe("/sensors/pressure", 1, &SimpleEstimator::pressureCallback, this);
+  m_depth_sub    = m_nh.subscribe("/sensors/depth", 1, &SimpleEstimator::pressureCallback, this);
   m_state_pub    = m_nh.advertise<nav_msgs::Odometry>("estimator/state", 1);
 
-  // Parameters used for calculating depth
-  if (!m_nh.getParam("atmosphere/pressure", m_atmospheric_pressure))
-    ROS_ERROR("Could not read parameter: atmosphere/pressure");
-
-  if (!m_nh.getParam("/water/density", m_water_density))
-    ROS_ERROR("Could not read parameter: /water/density");
-
-  if (!m_nh.getParam("/gravity/acceleration", m_gravitational_acceleration))
-    ROS_ERROR("Could not read parameter: /gravity/acceleration.");
-
   // Initiating orientation in quaternions
+  //  This is along the x-axs pointing forward from the body
   m_state.pose.pose.orientation.w = 1.0;
   m_state.pose.pose.orientation.x = 0.0;
   m_state.pose.pose.orientation.y = 0.0;
@@ -56,10 +47,9 @@ void SimpleEstimator::imuCallback(const sensor_msgs::Imu &msg)
   m_state_pub.publish(m_state);
 }
 
-void SimpleEstimator::pressureCallback(const sensor_msgs::FluidPressure &msg)
+void SimpleEstimator::pressureCallback(const std_msgs::Float64 &msg)
 {
-  const float gauge_pressure = msg.fluid_pressure - m_atmospheric_pressure;
-  const float depth_meters = gauge_pressure / (m_water_density * m_gravitational_acceleration);
+  const float depth_meters = msg.data;
   m_state.pose.pose.position.z = depth_meters;
   m_state_pub.publish(m_state);
 }
