cc0ebcb442777b526df22c63e6ea72954a648543
Aron <54807569+AronSW@users.noreply.github.com>
2021-04-30 10:49:12 +0200

    Update simple_estimator.h

--- vortex_estimator/include/vortex_estimator/simple_estimator.h
+++ vortex_estimator/include/vortex_estimator/simple_estimator.h
@@ -10,6 +10,7 @@
 #include "sensor_msgs/Imu.h"
 #include "std_msgs/Float64.h"
 #include "nav_msgs/Odometry.h"
+#include "vortex_msgs/Location.h"
 #include <string>
 
 class SimpleEstimator
@@ -18,11 +19,13 @@ class SimpleEstimator
     SimpleEstimator();
     void imuCallback(const sensor_msgs::Imu &msg);
     void pressureCallback(const std_msgs::Float64 &msg);
+    void locationCallback(const vortex_msgs/Location &msg);
     void publish();
   private:
     ros::NodeHandle m_nh;
     ros::Subscriber m_imu_sub;
     ros::Subscriber m_depth_sub;
+    ros::Subscriber m_location_sub;
     ros::Publisher  m_state_pub;
 
     bool m_ned_frame = false;
